87368b4b1e1764d46aa508a3c1bd8f6a25435661
Silvia0507 <silleon05@gmail.com>
2023-08-30 17:37:42 -0300

    Clase Libro.py

---
+++ Libro.py
@@ -0,0 +1,11 @@
+class Libro:
+	def_init_( self, 'titulo', 'autor', 'genero', 'puntuacion')
+	self.titulo = titulo
+	self.autor = autor
+	self.genero = genero
+	self.puntuacion = puntuacion
+
+lista_libro = [ 'titulo', 'autor', 'genero', 'puntuacion']
+	    
+
+
